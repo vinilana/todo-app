91f45c76842d809a593dc3362db3459dd0e8fc60
Vinicius Lana <viniciuslanadev@gmail.com>
2023-10-09 10:38:40 -0300

    Add navigation buttons

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useDrag } from "react-dnd";
+import { useBoardStore } from "../store/board.store";
 import { Task } from "../types";
 
 interface Props {
@@ -11,10 +12,30 @@ const Card: React.FC<Props> = ({ task }) => {
     type: "CARD",
     item: { id: task.id, status: task.status },
   });
+  const updateTaskStatus = useBoardStore((state) => state.updateTaskStatus);
+
+  const isFirstColumn = task.status === "todo";
+  const isLastColumn = task.status === "done";
+
+  const handleMoveBack = () => {
+    const status = task.status === "in-progress" ? "todo" : "in-progress";
+    updateTaskStatus(task.id, status as Task["status"]);
+  };
+
+  const handleMoveForward = () => {
+    const status = task.status === "in-progress" ? "done" : "in-progress";
+    updateTaskStatus(task.id, status as Task["status"]);
+  };
 
   return (
     <div ref={ref}>
       <h4>{task.title}</h4>
+      <button onClick={handleMoveBack} disabled={isFirstColumn}>
+        Move back
+      </button>
+      <button onClick={handleMoveForward} disabled={isLastColumn}>
+        Move forward
+      </button>
     </div>
   );
 };
